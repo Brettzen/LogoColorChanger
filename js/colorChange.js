4efd951e13c232f79a7324a5570b09074f6207b5
Brett Starks <brettz.starks@gmail.com>
2015-05-20 09:37:06 -0400

    logoRotate()
    added rotation for the logo

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,13 +1,34 @@
-var colorClass = ["blue", "red", "green", "gray"];
+var colorClass = ["red", "green", "blue", "gray"];
 var i = 0;
-
-function logoTurn() {
-	$("img").animate({
+var e = 360;
 
 
-	});
+// -------------------------Logo Animation-------------------------
+function rotationChange() {
+	if(e === 90) {
+		e = 360;
+	} else {
+		e -= 90;
+	}
 }
 
+function logoRotate() {
+	// $("img").animate({
+	rotationChange();
+	$('img').css({
+		'-webkit-transform': 'rotate(' + e + 'deg)',  //Safari 3.1+, Chrome  
+        '-moz-transform': 'rotate(' + e + 'deg)',     //Firefox 3.5-15  
+        '-ms-transform': 'rotate(' + e + 'deg)',      //IE9+  
+        '-o-transform': 'rotate(' + e + 'deg)',       //Opera 10.5-12.00  
+        'transform': 'rotate(' + e + 'deg)',          //Firefox 16+, Opera 12.50+  
+	});
+
+	// });
+}
+
+
+
+//------------------- Color Change--------------------------------
 function classChange() {
 	if(i > 2) { 
 		i = 0;
@@ -20,10 +41,12 @@ function colorChange() {
 	$('body').removeClass(colorClass[i]);
 	classChange();
 	$('body').addClass(colorClass[i]);
-	console.log('color is now ' + colorClass[i]);
-
+	
 }
 
 $('img').click(function() {
+	logoRotate();
+	console.log(e + 'degrees');
 	colorChange();
+	console.log('color is now ' + colorClass[i]);
 });
