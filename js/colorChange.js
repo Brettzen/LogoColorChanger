116ea3f6c69416a4041c8f55364617a8df81df39
Brett Starks <brettz.starks@gmail.com>
2015-05-20 08:47:00 -0400

    colorChange()
    Added basic color change function

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,6 +1,15 @@
 function logoTurn() {
 	$("img").animate({
-		
+
 
 	});
 }
+
+function colorChange() {
+	$('body').toggleClass("red");
+	console.log('color Changed');
+}
+
+$('img').click(function() {
+	colorChange();
+});
