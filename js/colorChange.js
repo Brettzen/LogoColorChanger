95aba8845ce77bc33c832fa85b953162c2e94f70
Brett Starks <brettz.starks@gmail.com>
2015-05-20 09:02:19 -0400

    colorChange()
    added more to colorChange(); gave it four different colors to cycle
    from.

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,3 +1,6 @@
+var colorClass = ["blue", "red", "green", "gray"];
+var i = 0;
+
 function logoTurn() {
 	$("img").animate({
 
@@ -5,9 +8,20 @@ function logoTurn() {
 	});
 }
 
+function classChange() {
+	if(i > 2) { 
+		i = 0;
+	} else {
+		i++;
+	}
+}
+
 function colorChange() {
-	$('body').toggleClass("red");
-	console.log('color Changed');
+	$('body').removeClass(colorClass[i]);
+	classChange();
+	$('body').addClass(colorClass[i]);
+	console.log('color is now ' + colorClass[i]);
+
 }
 
 $('img').click(function() {
